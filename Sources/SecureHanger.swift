0d142998b0bbf2ea67d2b56524849e5779567262
noppoman <yuki@miketokyo.com>
2016-08-05 15:53:01 +0900

    remove line for connection assignment

--- Sources/SecureHanger.swift
+++ Sources/SecureHanger.swift
@@ -18,7 +18,7 @@ extension Request {
 }
 
 public struct SecureHanger {
-    
+
     public init(connection: SecureClientConnection, request: Request, completion: ((Void) throws -> Response) -> Void) throws {
         if connection.closed {
             completion {
@@ -26,14 +26,14 @@ public struct SecureHanger {
             }
             return
         }
-        
+
         var request = request
         request.uri.scheme = "https"
         request.uri.host = connection.host
         if connection.port != 443 {
             request.uri.port = connection.port
         }
-        
+
         let future = QWFuture<Response> { (result: (() throws -> Response) -> ()) in
             result {
                 if !connection.opend {
@@ -42,7 +42,7 @@ public struct SecureHanger {
                 return try sendRequest(connection: connection, request: request)
             }
         }
-        
+
         future.onSuccess { response in
             completion {
                 response
@@ -59,24 +59,20 @@ public struct SecureHanger {
 
 private func sendRequest(connection: SecureClientConnection, request: Request) throws -> Response {
     var request = request
-    
+
     if request.userAgent == nil {
         request.userAgent = "SecureHanger HTTP Client"
     }
-    
+
     if request.host == nil {
         request.host = connection.host
     }
-    
-    if request.connection == nil {
-        request.connection = "Close"
-    }
-    
+
     let serializer = RequestSerializer()
     try serializer.serialize(request, to: connection)
-    
+
     let parser = ResponseParser()
-    
+
     while true {
         let data = try connection.receive()
         if let response = try parser.parse(data) {
